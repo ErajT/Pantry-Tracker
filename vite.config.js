da329a225e7904038c7879e57aaafd88e7df6337
Eraj Tanweer <erajtanweer@gmail.com>
2024-08-06 20:24:17 +0500

    first

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // This should be set to 'dist'
+    rollupOptions: {
+      input: {
+        main: 'src/main.jsx', // Ensure this points to your main entry file
+      },
+    },
+  },
+});
