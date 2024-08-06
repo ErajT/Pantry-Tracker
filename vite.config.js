07cf14211bdfae935625af5cd16edc61c590d8f4
Eraj Tanweer <erajtanweer@gmail.com>
2024-08-06 20:31:52 +0500

    hhh

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Pantry-Tracker/',
   plugins: [react()],
   build: {
     outDir: 'dist', // This should be set to 'dist'
